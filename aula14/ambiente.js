9c48e7c95abc3d7d0ac74ef3f7c92c04737f1e56
Luiz Carlos <luis.lc905.lc@gmail.com>
2023-05-08 11:56:14 -0300

    sla

---
+++ aula14/ambiente.js
@@ -0,0 +1,10 @@
+
+ /*while (c<=10) {
+    console.log(c)
+    c++
+ } */
+
+ for(var carros = 1; carros <=10; carros++) {
+    console.log(`você tem ${carros} carros.`)
+ }
+ console.log('Meus parabéns, você tem 10 carros')
